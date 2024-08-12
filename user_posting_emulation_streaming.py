0dc4c9b3369ed410b15a804d37b0620c7d0042b2
mariaalfar0 <treechomp@gmail.com>
2024-08-12 13:16:43 +0100

    Created user_posting_emulation_streaming.py file

---
+++ user_posting_emulation_streaming.py
@@ -0,0 +1,97 @@
+import requests
+from time import sleep
+import random
+from multiprocessing import Process
+import boto3
+import json
+import sqlalchemy
+from sqlalchemy import text
+import yaml
+import pymysql
+import datetime
+import pandas as pd
+
+
+random.seed(100)
+
+
+class AWSDBConnector:
+
+    def __init__(self):
+        with open('db_creds.yaml') as f:
+            db_creds = yaml.load(f, Loader=yaml.SafeLoader)
+        self.HOST = db_creds['RDS_HOST']
+        self.USER = db_creds['RDS_USER']
+        self.PASSWORD = db_creds['RDS_PASSWORD']
+        self.DATABASE = 'pinterest_data'
+        self.PORT = 3306
+        
+    def create_db_connector(self):
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        return engine
+
+
+new_connector = AWSDBConnector()
+
+def extract_data(engine, table_name, counter):
+    
+    with engine.connect() as connection:
+        selected_string = text(f"SELECT * FROM {table_name} LIMIT {counter}, 1")
+        selected_row = connection.execute(selected_string)
+            
+        for row in selected_row:
+            result = dict(row._mapping)
+        
+        return result
+    
+def post_to_api(invoke_url, data):
+    payload = json.dumps({
+        "records": [
+            {      
+            "value": data
+            }
+        ]
+    }, default = str)
+
+    headers = {'Content-Type': 'application/json'}
+    response = requests.request("PUT", invoke_url, headers=headers, data=payload)
+    print("Status code of response: ")
+    print(response.status_code)
+
+def query_db(engine, query):
+    with engine.connect() as connection:
+        query = text(f"{query}")
+        run_query = connection.execute(query)
+        
+        for row in run_query:
+            result = dict(row._mapping)
+        
+        return result
+
+def send_to_api():
+    counter = 0
+    engine = new_connector.create_db_connector()
+
+    row_count = query_db(engine, "SELECT COUNT(*) AS count FROM pinterest_data")
+    max_row_count = row_count["count"]
+    
+    while counter <= max_row_count:
+        pin_result = extract_data(engine, "pinterest_data", counter)
+        geo_result = extract_data(engine, "geolocation_data", counter)
+        user_result = extract_data(engine, "user_data", counter)
+
+        post_to_api("https://ez41mcd5n4.execute-api.us-east-1.amazonaws.com/0affe2a66fdf-stage/streams/streaming-0affe2a66fdf-pin/records", pin_result)
+        post_to_api("https://ez41mcd5n4.execute-api.us-east-1.amazonaws.com/0affe2a66fdf-stage/streams/streaming-0affe2a66fdf-geo/records", geo_result)
+        post_to_api("https://ez41mcd5n4.execute-api.us-east-1.amazonaws.com/0affe2a66fdf-stage/streams/streaming-0affe2a66fdf-user/records", user_result)
+
+        counter = counter + 1
+        print(f"{counter} out of {max_row_count}")     
+       
+
+if __name__ == "__main__":
+    send_to_api()
+
+    
+    
+
+
